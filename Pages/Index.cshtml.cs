ca2fe76c258276cf85808735f41d3c06626efb71
Jonas Rönnqvist <jonas.ronnqvist@iths.se>
2022-03-29 14:32:52 +0200

    added filters

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,13 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FPL_API.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Threading.Tasks;
-using FPL_API.Models;
 using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace FPL_API.Pages
 {
@@ -22,14 +21,29 @@ namespace FPL_API.Pages
 
         public HttpClient client = new HttpClient();
         public Statistic Statistic { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string TeamName { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Position { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public bool Bonus { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public bool Goals { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public bool Assists { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public bool PointsPerGame { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public bool TotalPoints { get; set; }
+        public List<string> Positions { get; set; }
 
         public async Task<IActionResult> OnGetAsync()
         {
             Statistic = await GetStatsAsync();
+            Positions = Statistic.element_types.Select(x => x.plural_name_short).Distinct().ToList();
 
             return Page();
         }
-
 
         public async Task<Statistic> GetStatsAsync()
         {
